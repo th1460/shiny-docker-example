0f404ebb23b80848583aad0447d84ed9a624c339
Thiago-Pires <Thiago.Pires@ibm.com>
2021-05-09 03:38:18 -0300

    feature: add actions

--- app.R
+++ app.R
@@ -4,9 +4,13 @@ library(shinydashboard)
 ui <- dashboardPage(
     dashboardHeader(title = "Hello World!"),
     dashboardSidebar(),
-    dashboardBody()
+    dashboardBody(
+        textOutput("text")
+    )
 )
 
-server <- function(input, output) { }
+server <- function(input, output) {
+    output$text <- renderText(Sys.getenv("TESTE1"))
+}
 
 shinyApp(ui, server)
